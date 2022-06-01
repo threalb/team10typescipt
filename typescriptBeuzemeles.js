1f72877ad16b75f4bb389728223c8d105be244f4
threalb_ <q.bencee@gmail.com>
2022-06-01 19:11:26 +0200

    változás történt

--- typescriptBeuzemeles.js
+++ typescriptBeuzemeles.js
@@ -1,1 +1,13 @@
-console.log("valami");
+var szam = 12;
+var szoveg = "banán";
+szoveg = "4.4";
+var logikari = true;
+logikari = false;
+//VESZÉLYES!!! Ha JS szintaxissal akarunk dolgozni
+var akarmi = false;
+akarmi = "alma";
+akarmi = 5;
+var ismeretlen = false;
+ismeretlen = "alma";
+ismeretlen = 5;
+console.log(Number(ismeretlen) + szam);
--- typescriptBeuzemeles.ts
+++ typescriptBeuzemeles.ts
@@ -1,1 +1,16 @@
-console.log("valami");
+var szam:number=12;
+var szoveg:string="banán";
+szoveg= "4.4";
+var logikari:boolean=true;
+logikari=false;
+
+//VESZÉLYES!!! Ha JS szintaxissal akarunk dolgozni
+var akarmi:any=false;
+akarmi="alma";
+akarmi=5;
+
+var ismeretlen:unknown=false;
+ismeretlen="alma";
+ismeretlen=5
+console.log(Number(ismeretlen)+szam);
+
